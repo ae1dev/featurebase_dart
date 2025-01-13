fd9479a98f0de6041ac55b8de04f5eaac5058e69
Elias <elias@deuss.dev>
2025-01-12 16:33:38 -0800

    feat(changelog): add fully fleshed out get endpoint

--- lib/src/endpoints/changelog.dart
+++ lib/src/endpoints/changelog.dart
@@ -6,16 +6,46 @@ class ChangelogEnd extends EndpointBase {
 
   ChangelogEnd(super.api);
 
-  /// Get a list of changelogs
+  /// Get changelogs
+  ///
+  /// This endpoint allows you to retrieve a paginated list of all your changelogs. By default, a maximum of ten changelogs are shown per page.
   Future<ResultsPagination<Changelog>> get({
+    /// The locale of the changelogs. Defaults to 'en'.
     String locale = 'en',
+
+    /// Page number.
     int page = 1,
+
+    /// Find changelog by its id.
+    String? id,
+
+    /// Search for changelogs by title or content.
+    String? q,
+
+    /// Filter changelogs by category, by providing an array of category names.
+    List<String>? categories,
+
+    /// Number of results per page. Maximum: 100.
+    int limit = 10,
+
+    /// The state of the changelog, either 'draft' or 'live'.
+    String state = 'live',
   }) async {
-    final Map<String, Object?> map = (await dio.get(_path, queryParameters: {
+    Map<String, dynamic> queryParams = {
       "locale": locale,
       "page": page,
-    }))
-        .data;
+    };
+
+    //Set Optional query params
+    if (id != null) queryParams["id"] = id;
+    if (q != null) queryParams["q"] = q;
+    if (categories != null) queryParams["categories"] = categories;
+
+    //Make request
+    final Map<String, Object?> map =
+        (await dio.get(_path, queryParameters: queryParams)).data;
+
+    //Parse response
     return ResultsPagination<Changelog>.fromJson(
       map,
       (json) => Changelog.fromJson(json as Map<String, dynamic>),
