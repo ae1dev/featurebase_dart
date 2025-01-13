d2c0cbaa15df4c583d32b9092679812dd0852f56
Elias <elias@deuss.dev>
2025-01-12 20:54:26 -0800

    chore: remove unused model

--- lib/src/models/collection.dart
+++ lib/src/models/collection.dart
@@ -62,29 +62,3 @@ class Collection with _$Collection {
   factory Collection.fromJson(Map<String, Object?> json) =>
       _$CollectionFromJson(json);
 }
-
-@freezed
-class CollectionContent with _$CollectionContent {
-  @JsonKey(includeFromJson: false)
-  const factory CollectionContent.article(Article article) =
-      _CollectionContentArticle;
-  @JsonKey(includeFromJson: false)
-  const factory CollectionContent.collection(Collection collection) =
-      _CollectionContentCollection;
-
-  factory CollectionContent.fromJson(Map<String, dynamic> json) {
-    if (json['type'] == 'article') {
-      return CollectionContent.article(Article.fromJson(json));
-    } else if (json['type'] == 'collection') {
-      return CollectionContent.collection(Collection.fromJson(json));
-    }
-    throw Exception('Unknown type for CollectionContent: ${json['type']}');
-  }
-
-  Map<String, dynamic> toJson() {
-    return when(
-      article: (article) => article.toJson(),
-      collection: (collection) => collection.toJson(),
-    );
-  }
-}
